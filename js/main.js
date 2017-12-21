216bb8efe0df8cfadc28fc2163c58f30eb1d5258
Julia <jullien.fall@gmail.com>
2017-12-21 22:57:05 +0100

    home-left

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
-function orderPageAppear(){
-  document.getElementById('order-page').style.display = "block"; 
+function orderPageAppear() {
+    document.getElementById('order-page').style.display = "block";
+    document.getElementById('price-page').style.display = "none";
+    document.getElementById('contact-page').style.display = "none";
+    document.getElementById('about-page').style.display = "none";
 }
 
-function orderPageClose(){
-  document.getElementById('order-page').style.display = "none";
+function orderPageClose() {
+    document.getElementById('order-page').style.display = "none";
+}
+
+function pricePageAppear() {
+    document.getElementById('price-page').style.display = "block";
+    document.getElementById('order-page').style.display = "none";
+    document.getElementById('contact-page').style.display = "none";
+    document.getElementById('about-page').style.display = "none";
+}
+
+function pricePageClose() {
+    document.getElementById('price-page').style.display = "none";
+}
+
+function contactPageAppear() {
+    document.getElementById('contact-page').style.display = "block";
+    document.getElementById('order-page').style.display = "none";
+    document.getElementById('price-page').style.display = "none";
+    document.getElementById('about-page').style.display = "none";
+}
+
+function contactPageClose() {
+    document.getElementById('contact-page').style.display = "none";
+}
+function aboutPageAppear() {
+    document.getElementById('about-page').style.display = "block";
+    document.getElementById('order-page').style.display = "none";
+    document.getElementById('price-page').style.display = "none";
+    document.getElementById('contact-page').style.display = "none";
+}
+function aboutPageClose() {
+    document.getElementById('about-page').style.display = "none";
 }
